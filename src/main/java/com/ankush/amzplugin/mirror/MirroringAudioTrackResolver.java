895ae3f531e6decf7d0fe641c5ff0bc0d10adb2f
whoami2005 <shyamsinghal175@gmail.com>
2026-06-26 16:49:13 +0530

    Delete src directory

--- src/main/java/com/ankush/amzplugin/mirror/MirroringAudioTrackResolver.java
+++
@@ -1,6 +0,0 @@
-package com.ankush.amzplugin.mirror;
-import com.sedmelluq.discord.lavaplayer.track.AudioItem;
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface MirroringAudioTrackResolver extends Function<MirroringAudioTrack, AudioItem> {}
